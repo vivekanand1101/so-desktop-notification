10bbf47f0911aa78a7d91091fee1cc92f7967b30
Vivek Anand <vivekanand1101@gmail.com>
2015-09-09 17:33:32 +0530

    Usage added

--- script.py
+++ script.py
@@ -1,23 +1,36 @@
-from sys import argv
+import sys
 import requests
 from bs4 import BeautifulSoup
-#from notify2 import notify2
 from time import sleep
 import notify2
+
 def send_message(title, message):
     notify2.init("Init")
     notice = notify2.Notification(title, message)
     notice.show()
     return
 
-url = "http://stackoverflow.com/feeds/tag?tagnames=%s&sort=newest" % argv[1]
-while True:
-    r = requests.get(url)
-    while r.status_code is not 200:
+def main(argv):
+    if len(sys.argv) < 2:
+        sys.stderr.write('Usage: python script.py <tag> \n')
+        sys.stderr.write('Ctrl-C to close the program \n')
+        return True
+
+    try:
+        url = "http://stackoverflow.com/feeds/tag?tagnames=%s&sort=newest" % sys.argv[1]
+        while True:
             r = requests.get(url)
-    soup = BeautifulSoup(r.text)
-    data = soup.find_all("link")
-    question = data[2].get('href')
-    question = question[question.find('questions') + 19:]
-    send_message("Question %s: " % argv[1].upper(), question)
-    sleep(60)
+            while r.status_code is not 200:
+                    r = requests.get(url)
+            soup = BeautifulSoup(r.text)
+            data = soup.find_all("link")
+            question = data[2].get('href')
+            question = question[question.find('questions') + 19:]
+            send_message("Question %s: " % sys.argv[1].upper(), question)
+            sleep(60)
+    except KeyboardInterrupt:
+        sys.stderr.write('\nBye \n')
+        return True
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
